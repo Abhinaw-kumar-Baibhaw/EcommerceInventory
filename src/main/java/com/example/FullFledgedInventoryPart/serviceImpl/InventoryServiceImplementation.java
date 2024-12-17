352ea8af7fbb023354ab28d5a08c57034c17d384
Abhinaw <abhinaw2801@gmail.com>
2024-12-17 15:45:14 +0530

    Inventory part

--- src/main/java/com/example/FullFledgedInventoryPart/serviceImpl/InventoryServiceImplementation.java
+++ src/main/java/com/example/FullFledgedInventoryPart/serviceImpl/InventoryServiceImplementation.java
@@ -3,11 +3,14 @@ package com.example.FullFledgedInventoryPart.serviceImpl;
 import com.example.FullFledgedInventoryPart.entities.ProductInventory;
 import com.example.FullFledgedInventoryPart.repo.InventoryRepo;
 import com.example.FullFledgedInventoryPart.service.InventoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
+
 
 @Service
 public class InventoryServiceImplementation implements InventoryService {
@@ -25,15 +28,21 @@ public class InventoryServiceImplementation implements InventoryService {
         return inventoryRepo.findAll();
     }
 
+    public static final Logger logger = LoggerFactory.getLogger(InventoryServiceImplementation.class);
+
+    @KafkaListener(topics = "order-created", groupId = "inventory-service-group")
+    public void message(String message){
+        logger.info("You are in consumer part in inventory" + message);
+    }
+
     @Override
-//    @KafkaListener(topics = "order-created", groupId = "inventory-service-group")
     public ProductInventory update(Long productId,ProductInventory productInventory) {
         Optional<ProductInventory> inventory = inventoryRepo.findById(productId);
         if(inventory.isPresent()){
             ProductInventory productInventory1 = inventory.get();
             productInventory1.setQuantity(productInventory.getQuantity());
             inventoryRepo.save(productInventory1);
-//            System.out.println("Received Order Event from kafka");
+            System.out.println("Received Order Event from kafka");
         }
         return productInventory;
     }
